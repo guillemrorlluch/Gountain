20e2a5075e124d4d24b6b6b876f172f8fa3aea6c
Guillermo <125073829+guillemrorlluch@users.noreply.github.com>
2025-08-09 23:02:31 +0200

    Update sw-v4.js

--- sw-v4.js
+++ sw-v4.js
@@ -1,3 +1,12 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
 const CACHE = 'rutas-botas-v4';
 const ASSETS = [
   './',
