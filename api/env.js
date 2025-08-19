fa34bc4a3738fa25662fff1c13e9188980886497
Guillermo <125073829+guillemrorlluch@users.noreply.github.com>
2025-08-19 19:51:31 +0200

    Delete api directory

--- api/env.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type','application/json; charset=utf-8');
-  res.setHeader('Cache-Control','no-store');
-  res.status(200).send(JSON.stringify({ MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '' }));
-}
